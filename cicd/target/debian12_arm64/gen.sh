699580fcac7e7a8467770453a5419e2aabfbfe07
JyJyJcr <82190170+JyJyJcr@users.noreply.github.com>
2024-03-28 22:27:18 +0900

    test: debian11

--- cicd/target/debian12_arm64/gen.sh
+++ cicd/target/debian12_arm64/gen.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-cargo binstall -y cargo-deb
-cargo deb
+cargo deb --variant 12
