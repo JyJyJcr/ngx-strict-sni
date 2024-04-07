440b01fa1b8b9051eba9d8b823f9ac7e855872e1
JyJyJcr <82190170+JyJyJcr@users.noreply.github.com>
2024-04-07 21:32:26 +0900

    fix: codename

--- cicd/common/debgen.sh
+++ cicd/common/debgen.sh
@@ -5,7 +5,7 @@ fi
 if [ -z "$2" ];then
     exit 71
 fi
-cargo deb --target $2 --variant $1
-for deb in $(ls target/debian|grep -E '\.deb$') ;do
-    cat /etc/os-release|grep VERSION_CODENAME|sed -e "s/^.*=//g" > target/debian/$deb.codename
+cargo deb --target "$2" --variant "$1"
+for deb in $(ls "target/$2/debian"|grep -E '\.deb$') ;do
+    cat /etc/os-release|grep VERSION_CODENAME|sed -e "s/^.*=//g" > "target/$2/debian/$deb.codename"
 done
